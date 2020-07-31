ae4e55f22788505e1d2e67b4126a61d4ba6bb23b
fatihtopcuoglu <vmax78@hotmail.com>
2020-07-31 09:47:52 +0200

    refactor - Android 'skip splahs screen - capabilities added'

--- src/main/java/nl/fatih/Platform/SessionFactory.java
+++ src/main/java/nl/fatih/Platform/SessionFactory.java
@@ -24,9 +24,12 @@ public class SessionFactory {
         capabilities.setCapability(MobileCapabilityType.NO_RESET, "false");
         capabilities.setCapability(MobileCapabilityType.FULL_RESET, "false");
         capabilities.setCapability(PLATFORM_NAME, "android");
+        capabilities.setCapability("appPackage", "com.example.fatihtaxcalculator"); // To start the the app skipping splashscreen
+        capabilities.setCapability("appActivity","com.example.fatihtaxcalculator.MainActivity");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9");
         capabilities.setCapability(AndroidMobileCapabilityType.AUTO_GRANT_PERMISSIONS, "true");
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
+
 //        capabilities.setCapability(MobileCapabilityType.APP, "/Users/fatihtopcuoglu/AndroidStudioProjects/FatihTaxCalculator/app/build/outputs/apk/debug/app-debug.apk");
         capabilities.setCapability(MobileCapabilityType.APP, "/Users/fatihtopcuoglu/AndroidStudioProjects/FatihTaxCalculator/app/build/outputs/apk/release/app-release-unsigned.apk");
         androidDriver = new AndroidDriver(url, capabilities);
